a2f11b6533367b7edc7a99f122bae92465521596
ImYeol <imyeol115@gmail.com>
2025-10-25 08:24:26 +0900

    remove firebase admin

--- src/lib/env.server.ts
+++ src/lib/env.server.ts
@@ -10,15 +10,6 @@ const requireEnv = (key: string) => {
 
 const supabaseServiceRoleKey = requireEnv("SUPABASE_SERVICE_ROLE_KEY");
 
-const firebaseProjectId = requireEnv("FIREBASE_PROJECT_ID");
-const firebaseClientEmail = requireEnv("FIREBASE_CLIENT_EMAIL");
-const firebasePrivateKey = requireEnv("FIREBASE_PRIVATE_KEY").replace(/\\n/g, "\n");
-
 export const serverEnv = {
   supabaseServiceRoleKey,
-  firebase: {
-    projectId: firebaseProjectId,
-    clientEmail: firebaseClientEmail,
-    privateKey: firebasePrivateKey,
-  },
 };
