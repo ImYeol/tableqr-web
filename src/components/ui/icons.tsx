eade803d1374f822533efbb2dc2d6a99c029c52c
ImYeol <imyeol115@gmail.com>
2025-10-27 23:28:01 +0900

    set order number as context

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -139,6 +139,23 @@ export const ShoppingBagIcon = createIcon(
 );
 ShoppingBagIcon.displayName = "ShoppingBagIcon";
 
+export const BellIcon = createIcon(
+  <>
+    <path d="M18 8a6 6 0 1 0-12 0c0 7-3 9-3 9h18s-3-2-3-9" />
+    <path d="M13.73 21a2 2 0 0 1-3.46 0" />
+  </>,
+);
+BellIcon.displayName = "BellIcon";
+
+export const TicketIcon = createIcon(
+  <>
+    <path d="M3 8a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v1.25a1.75 1.75 0 0 0 0 3.5V14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-1.25a1.75 1.75 0 0 0 0-3.5Z" />
+    <path d="M10 6v12" />
+    <path d="M14 6v12" />
+  </>,
+);
+TicketIcon.displayName = "TicketIcon";
+
 export const StarIcon = React.forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>(
   ({ className, ...props }, ref) => (
     <svg
---
+++ src/lib/formatQueueNumber.ts
@@ -0,0 +1,2 @@
+export const formatQueueNumber = (value: number): string => value.toString().padStart(2, "0");
+
